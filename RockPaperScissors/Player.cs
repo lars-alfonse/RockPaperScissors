42bdf71aa659d22ff1c5bc075c5ed7cce7c4109e
lars-alfonse <lars.alfonse@gmail.com>
2017-08-07 07:40:28 -0500

    added parent player class

---
+++ RockPaperScissors/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    abstract class Player
+    {
+        public int playerNumber;
+        public int playerSelection;
+        public int playerScore;
+
+        protected abstract void ChooseMove();
+    }
+}
