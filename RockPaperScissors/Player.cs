e7ceb9afcef3fce98e28fc7541e6442a2d77de85
lars-alfonse <lars.alfonse@gmail.com>
2017-08-07 08:24:42 -0500

    added robot child class with random generation, check scores added

--- RockPaperScissors/Player.cs
+++ RockPaperScissors/Player.cs
@@ -14,9 +14,38 @@ namespace RockPaperScissors
         protected string playerName;
 
         protected abstract void ChooseMove();
-        public void AddScore()
+        protected void AddScore()
         {
             playerScore += 1;
+        }
+        protected void compareSelection(Player opponent)
+        {
+            int scoreComparison;
+            scoreComparison = (5 + playerSelection + opponent.playerSelection) % 5;
+            if (scoreComparison == 0)
+            {
+                Console.WriteLine("Tie Game");
+            }
+            else if(scoreComparison % 2 == 0)
+            {
+                Console.WriteLine($"{playerName} loses the round!");
+            }
+            else
+            {
+                Console.WriteLine($"{playerName} wins the round!");
+                AddScore();
+            }
+        }
+        protected bool CheckScore()
+        {
+            if (playerScore > 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
---
+++ RockPaperScissors/Robot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Robot : Player
+    {
+        protected Random rnd = new Random();
+
+        public Robot(int playerNumber)
+        {
+            playerScore = 0;
+            this.playerNumber = playerNumber;
+            playerName = $"Computer Player {playerNumber}";
+        }
+        protected override void ChooseMove()
+        {
+            Console.WriteLine($"{playerName} will now select their choice");
+            playerSelection = rnd.Next(0, 5);
+        }
+    }
+}
