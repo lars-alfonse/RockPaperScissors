84e52af27778bd27c2bd671daf8e877aa858907d
lars-alfonse <lars.alfonse@gmail.com>
2017-08-07 08:35:57 -0500

    added game class, moved score and selection comparison to game class

--- RockPaperScissors/Player.cs
+++ RockPaperScissors/Player.cs
@@ -8,44 +8,16 @@ namespace RockPaperScissors
 {
     abstract class Player
     {
-        protected int playerNumber;
-        protected int playerSelection;
-        protected int playerScore;
-        protected string playerName;
+        public int playerNumber;
+        public int playerSelection;
+        public int playerScore;
+        public string playerName;
 
         protected abstract void ChooseMove();
-        protected void AddScore()
+        public void AddScore()
         {
             playerScore += 1;
         }
-        protected void compareSelection(Player opponent)
-        {
-            int scoreComparison;
-            scoreComparison = (5 + playerSelection + opponent.playerSelection) % 5;
-            if (scoreComparison == 0)
-            {
-                Console.WriteLine("Tie Game");
-            }
-            else if(scoreComparison % 2 == 0)
-            {
-                Console.WriteLine($"{playerName} loses the round!");
-            }
-            else
-            {
-                Console.WriteLine($"{playerName} wins the round!");
-                AddScore();
-            }
-        }
-        protected bool CheckScore()
-        {
-            if (playerScore > 2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+       
     }
 }
---
+++ RockPaperScissors/game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Game
+    {
+        public int numberOfHumanPlayers;
+
+        public Game()
+        {
+            numberOfHumanPlayers = GetHumanPlayers();
+        }
+        protected void compareSelection(Player playerOne, Player playerTwo)
+        {
+            int scoreComparison;
+            scoreComparison = (5 + playerOne.playerSelection + playerTwo.playerSelection) % 5;
+            if (scoreComparison == 0)
+            {
+                Console.WriteLine("Tie Game");
+            }
+            else if (scoreComparison % 2 == 0)
+            {
+                Console.WriteLine($"{playerTwo.playerName} wins the round!");
+                playerTwo.AddScore();
+            }
+            else
+            {
+                Console.WriteLine($"{playerOne.playerName} wins the round!");
+                playerOne.AddScore();
+            }
+        }
+        protected bool CheckScore(Player playerOne, Player playerTwo)
+        {
+            if (playerOne.playerScore > 2)
+            {
+                Console.WriteLine($"{playerOne.playerName} Wins The Game!!!");
+                return true;
+            }
+            else if (playerTwo.playerScore > 2)
+            {
+                Console.WriteLine($"{playerTwo.playerName} Wins The Game!!!");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
